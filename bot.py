6a1d638b2814558d67de74d69cf0911420569e23
pmaitland <pmaitland98@gmail.com>
2020-08-13 23:15:15 +0100

    Added roll command and moved constant strings

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import discord
+import random
+import strings
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,5 +12,45 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(strings.command_prefix):
+
+        words = message.content.lower().split(' ')
+        command = words[0][1:]
+
+        if command == 'roll':
+            response = roll(words)
+            await message.channel.send(response)
+            return
+        else:
+            await message.channel.send(strings.error % message.author.name)
+
+def roll(words):
+    if len(words) != 2:
+        return strings.roll_error
+
+    roll = words[1].split('d')
+
+    if len(roll) != 2:
+        return strings.roll_error
+
+    try:
+        dice = int(roll[1])
+        count = int(roll[0])
+
+        result = 0
+        for i in range(count):
+            result += random.randint(1, dice)
+        return strings.roll_result % result
+    except:
+    	try:
+    	    return strings.roll_result % random.randint(1, dice)
+    	except:
+            return strings.roll_error
 
 client.run(TOKEN)
---
+++ strings.py
@@ -0,0 +1,7 @@
+# common
+command_prefix = '!'
+error = 'I\'m sorry %s. I\'m afraid I can\'t do that.'
+
+# !roll
+roll_result = 'You rolled %s.'
+roll_error = 'Bad argument. Try `!roll 2d6` or `!roll d4`.'
